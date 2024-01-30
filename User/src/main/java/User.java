dd2efd9a05a38f278591ebfe388f668e7a25915b
AjayBandela <ajaybandela441@gmail.com>
2024-01-30 10:59:38 +0530

    update

---
+++ User/src/main/java/User.java
@@ -0,0 +1,23 @@
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String userName;
+
+    private int age;
+
+    private String mobNum;
+}
